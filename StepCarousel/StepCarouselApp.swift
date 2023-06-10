48fcf37d5eb2ee4c6e7c6ebf4f1db6cda2f8d953
umurgdk <umurgdk@gmail.com>
2023-06-11 02:01:28 +0300

    WIP: add custom transition carousel

---
+++ StepCarousel/StepCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepCarouselApp.swift
+//  StepCarousel
+//
+//  Created by Umur Gedik on 10.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct StepCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
